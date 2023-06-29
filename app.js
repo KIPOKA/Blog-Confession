2777784d03d25bd91d282230ed8270621cebe4fe
220825213 <220825213@tut4life.ac.za>
2023-06-29 09:14:22 +0200

    Done with front-end

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { title } = require('process');
+const morgan = require('morgan');
 
 
 //express app
@@ -7,24 +9,36 @@ const app= express();
 app.set('view engine', 'ejs');
 
 
-//listening for requuest
+
 app.listen(3000);
 
+//create a middleware
+
+app.use(morgan('dev'));
+
+
+//middleware of static files which cannot be show on the browser
+app.use(express.static('public'));
+//listening for request home
 app.get('/', (req, res) =>{
-            res.render('index');
+    const blogs=[
+        {title: "Break up", snippet:'Long distance'}, 
+        {title: "Cheating ", snippet:'Long distance end up in cheating'},
+        {title: "Congolese man ", snippet:'Give money instead of Love'}];
+            res.render('index', {title:"Home", blogs});
 });
+//listening for request about
 app.get('/about', (req, res) =>{
-    res.render('about');
+    res.render('about', {title:"About"});
 });
-
-
+  
 //redirects
 app.get('/blogs/create', (req, res) =>{
-    res.render('/create');
+    res.render('create', {title:"Create a new confession"});
 });
 
 //handle 404 page
 app.use((req, res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404', {title:"404"});
 
 })
